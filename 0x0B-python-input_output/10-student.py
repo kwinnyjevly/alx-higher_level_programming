eb6478e5ce84c8c6817eeb514387718cf701b9f9
kwinnyjevly <kwinnywrites57@gmail.com>
2023-02-23 17:24:24 -0800

    bling

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines a class Student."""
 
-
 class Student:
     """Represent a student."""
+
 
 def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
@@ -16,8 +16,7 @@ def __init__(self, first_name, last_name, age):
         self.last_name = last_name
         self.age = age
 
-
-def to_json(self, attrs=None):
+    def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
         If attrs is a list of strings, represents only those attributes
         included in the list.
